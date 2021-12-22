0fc7cde8ecf4513b173e13aa85c9e7f8e7dbb3bd
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-22 21:27:45 +0600

    Add files via upload

---
+++ demo of constractor short.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class student
+{
+public:
+
+    student(int id,double gpa)
+    {
+    cout<<"ID  "<<id<<"    "<<"GPA  "<<gpa<<endl;
+    }
+};
+int main()
+{
+    student shovo(20,3.55);
+    student rabbi(70,3.66);
+
+    getch();
+}
